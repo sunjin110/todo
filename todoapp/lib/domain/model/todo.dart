1160c2730531f965f83bf1779b00c507ee2de417
sunjin110 <sunjin110@gmail.com>
2023-08-10 10:34:54 +0900

    a

--- todoapp/lib/domain/model/todo.dart
+++ todoapp/lib/domain/model/todo.dart
@@ -1,6 +1,27 @@
 class Todo {
+  final TodoId id;
   final String title;
   final String description;
+  final DateTime createTime;
+  final DateTime? doneTime;
+  final TodoStatus status;
 
-  Todo({required this.title, required this.description});
+  Todo(
+      {required this.id,
+      required this.title,
+      required this.description,
+      required this.createTime,
+      required this.doneTime,
+      required this.status});
+}
+
+class TodoId {
+  final String id;
+  TodoId({required this.id});
+}
+
+enum TodoStatus {
+  done,
+  scheduled,
+  draft,
 }
--- todoapp/lib/domain/model/user.dart
+++ todoapp/lib/domain/model/user.dart
@@ -1,7 +1,17 @@
 class User {
+  final UserId id;
   final String name;
   final String email;
   final String password;
 
-  User({required this.name, required this.email, required this.password});
+  User(
+      {required this.id,
+      required this.name,
+      required this.email,
+      required this.password});
+}
+
+class UserId {
+  final String id;
+  UserId({required this.id});
 }
