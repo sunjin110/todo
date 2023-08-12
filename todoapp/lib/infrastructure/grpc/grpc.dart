b950bcbf1351d301734b8fe87e43ca46d432d592
sunjin110 <sunjin110@gmail.com>
2023-08-12 09:55:47 +0900

    a

--- todoapp/lib/infrastructure/grpc/grpc.dart
+++ todoapp/lib/infrastructure/grpc/grpc.dart
@@ -1,3 +1,11 @@
 import 'package:grpc/grpc.dart' as $grpc;
+import 'package:todoapp/infrastructure/grpc/proto_dart_gen/google/protobuf/timestamp.pb.dart';
+import 'dart:ffi';
 
 class GrpcClient {}
+
+DateTime convertTimestampToDateTime(Timestamp timestamp) {
+  return DateTime.fromMillisecondsSinceEpoch(
+      (timestamp.seconds * 1000 + (timestamp.nanos ~/ 1000000) as Int64) as int,
+      isUtc: true);
+}
---
+++ todoapp/lib/infrastructure/repository/authentication.dart
@@ -0,0 +1,64 @@
+import 'package:grpc/grpc.dart' as $grpc;
+import 'package:todoapp/domain/model/user_auth.dart' as $model;
+import 'package:todoapp/domain/repository/authentication.dart' as $repository;
+import 'package:todoapp/infrastructure/grpc/grpc.dart';
+import 'package:todoapp/infrastructure/grpc/proto_dart_gen/authentication/authentication.pb.dart';
+import 'package:todoapp/infrastructure/grpc/proto_dart_gen/authentication/authentication.pbgrpc.dart'
+    as $auth;
+
+class AuthenticationRepository implements $repository.AuthenticationRepository {
+  final $auth.AuthenticationClient client;
+
+  AuthenticationRepository({required this.client});
+
+  @override
+  Future<$model.Session> signIn(String email, String password) async {
+    var input = SignInInput();
+    input.email = email;
+    input.password = password;
+
+    final response = await this.client.signIn(input);
+
+    return convertToModelSession(response.session);
+  }
+
+  @override
+  Future<$repository.SignUpOutput> signUp(String email, String password) async {
+    var input = SignUpInput();
+    input.email = email;
+    input.password = password;
+
+    final res = await this.client.signUp(input);
+
+    return $repository.SignUpOutput(
+        session: convertToModelSession(res.session),
+        status: convertToModelUserSignUpStatus(res.status));
+  }
+
+  @override
+  Future<void> signOut($model.Session session) async {
+    throw UnimplementedError();
+  }
+}
+
+$model.Session convertToModelSession(Session session) {
+  return $model.Session(
+      session: session.session,
+      expireTime: convertTimestampToDateTime(session.expireTime));
+}
+
+$model.UserSignUpStatus convertToModelUserSignUpStatus(
+    UserSignUpStatus status) {
+  switch (status) {
+    case UserSignUpStatus.SignupSignUpStatusAllowed:
+      return $model.UserSignUpStatus.allowed;
+    case UserSignUpStatus.SignupSignUpStatusDenied:
+      return $model.UserSignUpStatus.denied;
+    case UserSignUpStatus.SignupSignUpStatusWaitForAllow:
+      return $model.UserSignUpStatus.waitForAllow;
+    case UserSignUpStatus.SignupSignUpStatusUnknown:
+      throw UnimplementedError("specified unknown sign up status");
+    default:
+      throw UnimplementedError("not implemented sign up status");
+  }
+}
