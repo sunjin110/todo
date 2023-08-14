dbe4983610ec29c2903b71673efe14cde5e797eb
sunjin110 <sunjin110@gmail.com>
2023-08-14 09:54:29 +0900

    a

---
+++ todoapp/lib/domain/repository/session.dart
@@ -0,0 +1,7 @@
+import 'package:todoapp/domain/model/user_auth.dart' as $model;
+
+abstract class SessionRepository {
+  Future<$model.Session> get();
+  Future<void> set($model.Session session);
+  Future<void> delete();
+}
