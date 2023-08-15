733f3c8b247886f179c3306f809dc30a86214274
sunjin110 <sunjin110@gmail.com>
2023-08-15 15:39:14 +0900

    a

--- todoapp/lib/infrastructure/repository/session.dart
+++ todoapp/lib/infrastructure/repository/session.dart
@@ -1,6 +1,7 @@
 import 'package:todoapp/domain/model/user_auth.dart' as $model;
 import 'package:todoapp/domain/repository/session.dart' as $repository;
 import 'package:realm/realm.dart';
+import 'package:todoapp/infrastructure/repository/dto/session.dart' as $dto;
 
 const _key = "my_session_key";
 
@@ -9,20 +10,25 @@ class SessionRepository implements $repository.SessionRepository {
   SessionRepository(this.realm);
 
   @override
-  Future<void> delete() {
-    // TODO: implement delete
-    throw UnimplementedError();
+  Future<void> delete() async {
+    // 1recordしか入らないのでこれでいい
+    realm.deleteAll<$dto.Session>();
   }
 
   @override
-  Future<$model.Session> get() {
-    // TODO: implement get
-    throw UnimplementedError();
+  Future<$model.Session?> get() async {
+    final result = realm.query<$dto.Session>(_key);
+    final sessionDto = result.elementAtOrNull(0);
+    if (sessionDto == null) {
+      return null;
+    }
+    return $model.Session(
+        session: sessionDto.session, expireTime: sessionDto.expireTime);
   }
 
   @override
-  Future<void> set($model.Session session) {
-    // TODO: implement set
-    throw UnimplementedError();
+  Future<void> set($model.Session session) async {
+    final sessionDto = $dto.Session(_key, session.session, session.expireTime);
+    realm.add(sessionDto);
   }
 }
